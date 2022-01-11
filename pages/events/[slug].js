5d3fb3c0e1f0ef32fc8fb6a9bfd80d1c1625eb21
Zeeshan Haider <zeeshanhshaheen@gmail.com>
2022-01-11 23:04:52 +0100

    seting up pages

---
+++ pages/events/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleEvent = () => {
+  return (
+    <div>
+      <h1>My Events</h1>
+    </div>
+  );
+};
+
+export default SingleEvent;
---
+++ pages/events/add.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddEvents = () => {
+  return (
+    <div>
+      <h1>Add Events</h1>
+    </div>
+  );
+};
+
+export default AddEvents;
